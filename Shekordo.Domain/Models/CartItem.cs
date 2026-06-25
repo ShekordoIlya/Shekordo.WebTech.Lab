49f24f3a83fad30a89f03870b0b6822348bbf00f
Ilya <ilya2703@bk.ru>
2026-06-25 17:34:01 +0300

    Delete Shekordo.Domain directory

--- Shekordo.Domain/Models/CartItem.cs
+++
@@ -1,9 +0,0 @@
-using Shekordo.Domain.Entities;
-
-namespace Shekordo.Domain.Models;
-
-public class CartItem
-{
-    public Dish Item { get; set; } = default!;
-    public int Qty { get; set; }
-}
--- Shekordo.Domain/Models/ListModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shekordo.Domain.Models;
-
-public class ListModel<T>
-{
-    public List<T> Items { get; set; } = new();
-    public int CurrentPage { get; set; } = 1;
-    public int TotalPages { get; set; } = 1;
-}
